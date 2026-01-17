1b8d689bfaba5d9d04882780f46dfe131a3c1bbd
shriyapsawant21-wq <shriyapsawant.21@gmail.com>
2026-01-18 00:23:19 +0530

    ability
    removed old ability, new ability time reverse added

---
+++ Assets/Scripts/Abiility.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ability : MonoBehaviour  // Class name MUST match filename
+{
+    [SerializeField] private KeyCode abilityKey = KeyCode.LeftShift;
+    [SerializeField] private float rewindDuration = 3f;
+    
+    private List<Vector3> positions = new List<Vector3>();
+    private bool isRewinding = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(abilityKey))
+            StartRewind();
+        
+        if (Input.GetKeyUp(abilityKey))
+            StopRewind();
+    }
+    
+    void FixedUpdate()
+    {
+        if (isRewinding && positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);
+        }
+        else if (!isRewinding)
+        {
+            positions.Insert(0, transform.position);
+            if (positions.Count > Mathf.RoundToInt(rewindDuration / Time.fixedDeltaTime))
+                positions.RemoveAt(positions.Count - 1);
+        }
+    }
+    
+    void StartRewind()
+    {
+        isRewinding = true;
+        Debug.Log("Rewinding time...");
+    }
+    
+    void StopRewind()
+    {
+        isRewinding = false;
+        Debug.Log("Stopped rewinding");
+    }
+}
