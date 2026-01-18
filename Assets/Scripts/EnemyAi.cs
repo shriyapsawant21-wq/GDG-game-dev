3c871afe9437d5e1958e667f8cf3f0ef32235576
shriyapsawant21-wq <shriyapsawant.21@gmail.com>
2026-01-18 12:21:25 +0530

    EnemyAI
    Creating Enemy Attack script

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -2,5 +2,54 @@ using UnityEngine;
 
 public class EnemyAi
 {
-    [SerializeField] 
+    [Header("Movement")]
+    [SerializeField] private float BossSpeed=2f;
+    [SerializeField] private float detectionRange=8f;
+    [SerializeField] private float AttackRange=0.5f;
+    
+
+    private Transform Player;
+    private bool PlayerDetected =false;
+    private bool CanAttack=true;
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        Player=GameObject.FindGAmeObjectWithTag("Player")?.transform;
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position,Player.position);
+
+        if(distance<=detectionRange)
+        {
+            PlayerDetected=true;
+
+        }
+
+        if(PlayerDetected)
+        {
+            FollowPLayer();
+
+            if(distance<=AttackRange&&CanAttack)
+            {
+                AttackPlayer();
+            }
+        }
+    }
+
+    void FollowPLayer()
+    {
+        
+    }
+
+    void AttackPlayer()
+    {
+        
+    }
+
+
 }
